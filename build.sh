f8af239a9c416ea9939869818b2ccc29cd499726
Alex Mingoia <alex@alexmingoia.com>
2022-08-20 11:35:09 +0630

    Add layout for articles.

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-rm -rf publish
-mkdir -p publish/blog
-cd md
-find . -type f -name '*.md' -exec sh -c 'mmark -t ../layout.mustache -i {} -o ../publish/$(basename {} .md).html' \;
-cd ..
+mkdir -p publish
+rm -rf publish/*
+find . -maxdepth 1 -type f -name '*.md' -exec sh -c 'mmark -t layout.mustache -i {} -o publish/$(basename {} .md).html' \;
 npx tailwindcss -i main.css -o publish/main.min.css --minify
 git checkout gh-pages && mv publish/* . && git add . && git commit -m "automatic update" && git checkout main
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["publish/**/*.html"],
+  content: ["*.mustache"],
   theme: {
     extend: {},
   },
